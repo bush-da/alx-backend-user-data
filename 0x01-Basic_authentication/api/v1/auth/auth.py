3b1957bc74bed7780f277718a78979b917fba099
Daniel Tujuma <danielsevani12@gmail.com>
2024-11-14 22:47:42 +0300

    fix minor issue

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python3
-"""Manage the API authentication"""
 from flask import request
-from collection import TypeVar, List
+from typing import List, TypeVar
 
 
-class Auth():
-    """Authentication"""
+class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Return false"""
+        """
+        Determines if authentication is required for a given path.
+        Currently, this always returns False, as `path` and `excluded_paths`
+        will be used later for more specific behavior.
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
-        """Authorization header"""
+        """
+        Retrieves the authorization header from the Flask request object.
+        Currently returns None, will be implemented later.
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """current user"""
+        """
+        Retrieves the current user based on the Flask request object.
+        Currently returns None, implementation to be added later.
+        """
         return None
