faa8b68c3f98af6c3577be55dd5abee41b7faa7f
Daniel Tujuma <danielsevani12@gmail.com>
2024-11-14 18:02:08 +0300

    handle 401 error

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,17 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def about() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - raise a 401 error
+    """
+    abort(401)
+
+
+
+
+
