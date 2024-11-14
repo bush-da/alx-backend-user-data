a07a271aec0f5cae5ff28600d6503b2f57301a94
Daniel Tujuma <danielsevani12@gmail.com>
2024-11-14 18:27:11 +0300

    handle forbidden errors

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauth() -> str:
       - raise a 401 error
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+       - abort a 403 error
+    """
+    abort(403)
