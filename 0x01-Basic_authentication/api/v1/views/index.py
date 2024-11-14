a34fcf661827ee8054efc09b7201a0112cdd7c28
Daniel Tujuma <danielsevani12@gmail.com>
2024-11-14 18:06:44 +0300

    fix func name

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -27,7 +27,7 @@ def stats() -> str:
 
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def abort() -> str:
+def unauth() -> str:
     """ GET /api/v1/unauthorized
     Return:
       - raise a 401 error
