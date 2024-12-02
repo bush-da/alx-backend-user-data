a5ac07f9f96a3ae940361a4ddc0dfa076a0bfc89
Daniel Tujuma <danielsevani12@gmail.com>
2024-12-03 01:15:04 +0300

    create session id for user

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,7 +2,21 @@
 """This module defines a class the will be used for session auth
 """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
-    """do noting"""
+    """Defines session based auth
+    """
+
+    user_id_by_session_id = dict()
+
+    def create_session(self, user_id: str = None) -> str:
+        """method creates a session id for user with uuid4
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+        
